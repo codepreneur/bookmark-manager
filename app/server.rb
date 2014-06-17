ac75bf6f4cab56ca5cae812b1aceeb46cd24da92
gomenasaai <contramundee@gmail.com>
2014-06-17 15:56:32 +0100

    preventing duplicate registrations

--- app/server.rb
+++ app/server.rb
@@ -39,21 +39,20 @@ end
 
 get '/users/new' do
 	@user = User.new
-
 	erb :"users/new"
 end
 
 post '/users' do
-	user = User.create(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
-	session[:user_id] = user.id
-	if user.save
-		session[:user_id] = user.id
+	@user = User.new(:email => params[:email], :password => params[:password], :password_confirmation => params[:password_confirmation])
+
+	if @user.save
+		session[:user_id] = @user.id
 		redirect to('/')
 	else
-		flash[:notice] = "Sorry, your passwords dont match"
-		@user = user
+		flash.now[:errors] = @user.errors.full_messages
 		erb :"users/new"
 	end
+	
 end
 
 
