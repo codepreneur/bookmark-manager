36fa01f803a739cce19ccca02c8c3bbce2cb83f0
gomenasaai <contramundee@gmail.com>
2014-06-17 11:16:14 +0100

    added password confirmation/checking before saving to database

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,9 +1,10 @@
 
 env = ENV["RACK_ENV"] || "development"
+DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
