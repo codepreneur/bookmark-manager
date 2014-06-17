87a046fb45fe9217edd8306fdc3d5af942afb3a2
gomenasaai <contramundee@gmail.com>
2014-06-17 23:34:16 +0100

    refactored code, adding user accounts - signing out

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,21 @@
 require 'spec_helper'
+require_relative 'helpers/session'
+include SessionHelpers
+
+feature "User signs out" do
+
+	before(:each) do
+		User.create(:email => "test@test.com", :password => 'test', :password_confirmation => 'test')
+	end
+
+	scenario "while being signed in" do
+		sign_in('test@test.com', 'test')
+		click_button "Sign out"
+		expect(page).to have_content("Good Bye!") 
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
+end
 
 feature "User signs in" do
 
@@ -20,12 +37,6 @@ feature "User signs in" do
 		expect(page).not_to have_content("Welcome, test@test.com")
 	end
 
-	def sign_in(email,password)
-		visit '/sessions/new'
-		fill_in 'email', :with => email
-		fill_in 'password', :with => password
-		click_button 'Sign in'
-	end
 
 end
 
@@ -49,12 +60,5 @@ feature "User signs up" do
 		expect(page).to have_content("This email is already taken")
 	end
 
-	def sign_up(email = "alice@example.com", password = "oranges!", password_confirmation = "oranges!")
-		visit '/users/new'
-		fill_in :email, :with => email
-		fill_in :password, :with => password
-		fill_in :password_confirmation, :with => password_confirmation
-		click_button "Sign up"
-	end
 
 end
